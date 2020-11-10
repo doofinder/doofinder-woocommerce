6c20957d4f12c18401d2f193b51d4286844deb59
Carlos Escribano Rey <carlos@doofinder.com>
2020-11-10 09:41:38 +0100

    fix: add missing vendor files

---
+++ doofinder-for-woocommerce/lib/cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pushd vendor
+find . -name "*.md" -print0 | xargs -0 rm
+find . -name "Dockerfile" -print0 | xargs -0 rm
+find . -name "*.json" -print0 | xargs -0 rm
+find . -name "*.pubkey*" -print0 | xargs -0 rm
+find . -name "*.php_cs*" -print0 | xargs -0 rm
+find . -name "phpstan*" -print0 | xargs -0 rm
+find . -name "Makefile" -print0 | xargs -0 rm
+find . -name "*.xml" -print0 | xargs -0 rm
+popd
