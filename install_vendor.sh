61c3e1fa11ab2a21d076758df9fe92da06557c2a
Carlos Escribano Rey <carlos@doofinder.com>
2020-11-10 10:03:58 +0100

    refactor: script to install doofinder deps and cleanup

--- install_vendor.sh
+++ install_vendor.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+pushd doofinder-for-woocommerce/lib
+composer install
+
 pushd vendor
 find . -name "*.md" -print0 | xargs -0 rm
 find . -name "Dockerfile" -print0 | xargs -0 rm
@@ -8,4 +12,6 @@ find . -name "*.php_cs*" -print0 | xargs -0 rm
 find . -name "phpstan*" -print0 | xargs -0 rm
 find . -name "Makefile" -print0 | xargs -0 rm
 find . -name "*.xml" -print0 | xargs -0 rm
+popd
+
 popd
