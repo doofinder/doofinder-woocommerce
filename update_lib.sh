cac0922ccc800b841111b840a9f0bbc3ca3bb5f0
Carlos Escribano Rey <carlos@doofinder.com>
2021-04-15 14:39:38 +0200

    refactor: update lib script

--- update_lib.sh
+++ update_lib.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-pushd doofinder-for-woocommerce/lib
-rm -rf swagger vendor .git src/Search/Test
-rm .gitignore .travis.yml CHANGELOG.md index.php NOTAS phpunit.xml
-composer install --no-dev
+function check_commands {
+  for app in "curl" "jq";
+  do
+    if ! [ -x "$(command -v $app)" ]; then
+      echo "Error: $app is not installed." >&2
+      exit 1
+    fi
+  done
+}
 
+check_commands
+pushd doofinder-for-woocommerce
+
+rm -rf lib
+DOWNLOAD_URL=`curl -s https://api.github.com/repos/doofinder/php-doofinder/releases/latest | jq -r ".tarball_url"`
+curl -L $DOWNLOAD_URL | tar xzv
+mv `find . -type d -name doofinder-php-doofinder-*` lib
+
+pushd lib
+
+composer update --no-dev
+
+rm -rf .gitignore swagger .git src/Search/Test
+rm .travis.yml CHANGELOG.md index.php NOTAS phpunit.xml
 find . -name "docs" -type d -print0 | xargs -0 rm -rf
 find . -name ".github" -type d -print0 | xargs -0 rm -rf
 find . -name "*.md" -print0 | xargs -0 rm -rf
 
 pushd vendor
+
 find . -name "*.md" -print0 | xargs -0 rm
 find . -name "Dockerfile" -print0 | xargs -0 rm
 find . -name "*.json" -print0 | xargs -0 rm
@@ -18,6 +38,7 @@ find . -name "*.php_cs*" -print0 | xargs -0 rm
 find . -name "phpstan*" -print0 | xargs -0 rm
 find . -name "Makefile" -print0 | xargs -0 rm
 find . -name "*.xml" -print0 | xargs -0 rm
-popd
 
+popd
+popd
 popd
