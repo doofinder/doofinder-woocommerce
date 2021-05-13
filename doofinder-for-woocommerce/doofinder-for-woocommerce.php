7c0461e49b24b2b39d9e97b74d4dbfa4b4d952a3
Carlos Escribano Rey <carlos@doofinder.com>
2021-05-13 09:14:03 +0200

    build: v1.4.3

--- doofinder-for-woocommerce/doofinder-for-woocommerce.php
+++ doofinder-for-woocommerce/doofinder-for-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: Doofinder for WooCommerce
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 1.4.2
+ * Version: 1.4.3
  * Author: doofinder
  * Description: Integrate Doofinder Search in your WooCommerce shop.
  * WC requires at least: 2.1.0
@@ -42,7 +42,7 @@ if (
 			 *
 			 * @var string
 			 */
-			public static $version = '1.4.2';
+			public static $version = '1.4.3';
 
 			/**
 			 * The only instance of Doofinder_For_WooCommerce
