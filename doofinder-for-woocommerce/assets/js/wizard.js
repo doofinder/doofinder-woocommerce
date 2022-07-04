adf143b1bace0724d0d09a24a3794a54e9533487
Pedro Cruz <pedro@doofinder.com>
2022-07-04 14:43:53 +0100

    Added checkdata errors

--- doofinder-for-woocommerce/assets/js/wizard.js
+++ doofinder-for-woocommerce/assets/js/wizard.js
@@ -1,87 +1,118 @@
 jQuery(() => {
-    const $ = jQuery.noConflict();
+  const $ = jQuery.noConflict();
 
-    let connectModal;
-    let checkInterval;
+  let connectModal;
+  let checkInterval;
 
-    const popupCenter = ({ url, title, w, h }) => {
-      const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : window.screenX;
-      const dualScreenTop = window.screenTop !== undefined ? window.screenTop : window.screenY;
-      const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-      const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
-      const systemZoom = width / window.screen.availWidth;
-      const left = (width - w) / 2 / systemZoom + dualScreenLeft;
-      const top = (height - h) / 2 / systemZoom + dualScreenTop;
-      const newWindow = window.open(url, title, `
+  const popupCenter = ({ url, title, w, h }) => {
+    const dualScreenLeft =
+      window.screenLeft !== undefined ? window.screenLeft : window.screenX;
+    const dualScreenTop =
+      window.screenTop !== undefined ? window.screenTop : window.screenY;
+    const width = window.innerWidth
+      ? window.innerWidth
+      : document.documentElement.clientWidth
+      ? document.documentElement.clientWidth
+      : screen.width;
+    const height = window.innerHeight
+      ? window.innerHeight
+      : document.documentElement.clientHeight
+      ? document.documentElement.clientHeight
+      : screen.height;
+    const systemZoom = width / window.screen.availWidth;
+    const left = (width - w) / 2 / systemZoom + dualScreenLeft;
+    const top = (height - h) / 2 / systemZoom + dualScreenTop;
+    const newWindow = window.open(
+      url,
+      title,
+      `
         scrollbars=yes,
         width=${w / systemZoom},
         height=${h / systemZoom},
         top=${top},
         left=${left}
-      `);
+      `
+    );
 
-      if (window.focus) {
-        newWindow.focus();
-      }
+    if (window.focus) {
+      newWindow.focus();
+    }
 
-      return newWindow;
-    };
+    return newWindow;
+  };
 
-    const confirmLeavePage = (active = true) => {
-      if (active) {
-        window.onbeforeunload = () => '';
-      } else {
-        window.onbeforeunload = () => null;
-      }
-    };
+  const confirmLeavePage = (active = true) => {
+    if (active) {
+      window.onbeforeunload = () => "";
+    } else {
+      window.onbeforeunload = () => null;
+    }
+  };
 
-    const ajaxCheckData = () => {
-      jQuery.ajax({
-        type: 'POST',
-        dataType: 'json',
+  const ajaxCheckData = () => {
+    jQuery
+      .ajax({
+        type: "POST",
+        dataType: "json",
         url: DoofinderForWC.ajaxUrl,
-        data: { action: 'doofinder_for_wc_check_data' }
-      }).then((response) => {
+        data: { action: "doofinder_for_wc_check_data" },
+      })
+      .then((response) => {
         if (!response.success) {
-            console.error('ajaxCheckData - Something went wrong');
-            return;
+          console.error("ajaxCheckData - Something went wrong");
+          return;
         }
 
-        console.info('ajaxCheckData - Status: ' + response.data.status);
+        console.info("ajaxCheckData - Status: " + response.data.status);
 
-        if (response.data.status !== 'saved' && response.data.status !== 'error') {
-            console.info('ajaxCheckData - checking again...');
-            return;
+        if (
+          response.data.status !== "saved" &&
+          response.data.status !== "error"
+        ) {
+          console.info("ajaxCheckData - checking again...");
+          return;
         }
 
-        clearInterval(checkInterval);
+        $(".errors-wrapper ul").remove();
+        if (response.data.status === "error") {
+          errors = "<ul>";
+          for (er in response.data.error) {
+            error = response.data.error[er];
+            errors += "<li>" + error + "</li>";
+          }
+          errors += "</ul>";
+          $(".errors-wrapper").append(errors);
+          $(".errors-wrapper").show();
+        }
 
-        confirmLeavePage(false);
-
-        console.info('ajaxCheckData - reloading page...');
-
-        window.location.href = doofinderSetupWizardUrl;
+        if (response.data.status === "saved") {
+          clearInterval(checkInterval);
+          confirmLeavePage(false);
+          console.info("ajaxCheckData - reloading page...");
+          window.location.href = doofinderSetupWizardUrl;
+        }
       });
-    };
+  };
 
-    $('#doofinder-for-wc-index-button').click(() => {
-      const progressBarWrapper = $('.dfwc-setup-step__progress-bar-wrapper').get(0);
-      progressBarWrapper.classList.add('active');
-      progressBarWrapper.parentElement.classList.add('indexing');
+  $("#doofinder-for-wc-index-button").click(() => {
+    const progressBarWrapper = $(".dfwc-setup-step__progress-bar-wrapper").get(
+      0
+    );
+    progressBarWrapper.classList.add("active");
+    progressBarWrapper.parentElement.classList.add("indexing");
+  });
+
+  $(".open-window").click((e) => {
+    const pageType = $(e.currentTarget).attr("data-type");
+
+    connectModal = popupCenter({
+      url: `https://app.doofinder.com/plugins/${pageType}/woocommerce?email=${doofinderConnectEmail}&token=${doofinderConnectToken}&return_path=${doofinderConnectReturnPath}`,
+      title: "DoofinderConnect",
+      w: 600,
+      h: 700,
     });
 
-    $('.open-window').click(e => {
-      const pageType = $(e.currentTarget).attr('data-type');
-
-      connectModal = popupCenter({
-          url: `https://app.doofinder.com/plugins/${pageType}/woocommerce?email=${doofinderConnectEmail}&token=${doofinderConnectToken}&return_path=${doofinderConnectReturnPath}`,
-          title: 'DoofinderConnect',
-          w: 600,
-          h: 700
-      });
-
-      clearInterval(checkInterval);
-      checkInterval = setInterval(ajaxCheckData, 500);
-    });
-
+    clearInterval(checkInterval);
+    checkInterval = setInterval(ajaxCheckData, 1000);
+  });
 });
--- doofinder-for-woocommerce/views/wizard-step-2.php
+++ doofinder-for-woocommerce/views/wizard-step-2.php
@@ -13,5 +13,8 @@ use Doofinder\WC\Setup_Wizard;
 <div class="dfwc-setup-step__actions">
     <a class="button button-primary open-window" data-type="login" href="#"><?php _e('Log in', 'woocommerce-doofinder'); ?></a>
     <a class="button button-primary open-window" data-type="signup" href="#"><?php _e('Sign up', 'woocommerce-doofinder'); ?></a>
-    <a href="<?php echo Settings::get_url(); ?>" class="dfwc-setup-skip"><?php _e('Skip and exit setup','woocommerce-doofinder'); ?></a>
+</div>
+
+<div class="errors-wrapper doofinder-for-wc-indexing-error" style="display:none">
+    <h3 style="color: #cc0000;"><?php _e('An error ocurred while connecting with doofinder', 'woocommerce-doofinder'); ?> :</h3>
 </div>
