83b527b9db61eef5bd8894c171f7a6ce6cd0ba60
Pedro Cruz <pedro@doofinder.com>
2023-08-24 14:00:52 +0100

    Improve REST API Handler (#184)
    * Improve rest api handler
    
    * Bump up version

--- doofinder-for-woocommerce/doofinder-for-woocommerce.php
+++ doofinder-for-woocommerce/doofinder-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name: Doofinder WP & WooCommerce Search
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 2.0.14
+ * Version: 2.0.15
  * Author: Doofinder
  * Description: Integrate Doofinder Search in your WordPress site or WooCommerce shop.
  *
@@ -34,7 +34,7 @@ if (!class_exists('\Doofinder\WP\Doofinder_For_WordPress')) :
          *
          * @var string
          */
-        public static $version = '2.0.14';
+        public static $version = '2.0.15';
 
         /**
          * The only instance of Doofinder_For_WordPress
@@ -137,8 +137,6 @@ if (!class_exists('\Doofinder\WP\Doofinder_For_WordPress')) :
                 } else {
                     Admin_Notices::remove_notice($old_plugin_notice_name);
                 }
-
-                REST_API_Handler::initialize();
             });
 
             add_action('plugins_loaded', array($class, 'plugin_update'));
@@ -338,58 +336,8 @@ if (!class_exists('\Doofinder\WP\Doofinder_For_WordPress')) :
         {
             add_action('rest_api_init', function () {
                 Config::register();
-                register_rest_route('doofinder/v1', '/index-status', array(
-                    'methods' => 'POST',
-                    'callback' => function (\WP_REST_Request $request) {
-                        $log = new Log('index-status.log');
-                        $log->log("Received indexing status request with payload:\n" . print_r($request, true));
-                        $valid_message = "Sources were processed successfully.";
-                        if ($request->get_param('token') != Settings::get_api_key()) {
-                            return new WP_REST_Response(
-                                [
-                                    'status' => 401,
-                                    'response' => "Invalid token"
-                                ],
-                                401
-                            );
-                        }
-
-                        $error_message = $request->get_param('message');
-                        if (!empty($error_message) && $error_message != $valid_message) {
-                            $notice_title = __("An error has occurred while indexing your catalog", "wordpress-doofinder");
-                            $notice_content = __("To obtain further details, you can check the indexing results by accessing the \"Indices\" section in your Doofinder admin panel. If the problem persists, please contact our support team at <a href=\"mailto:support@doofinder.com\">support@doofinder.com</a>", "wordpress-doofinder");
-                            //Dismiss the indexing notice as it has already finished
-                            Setup_Wizard::dismiss_indexing_notice();
-                            Admin_Notices::add_notice("indexing-status-failed", $notice_title, $notice_content, 'error', null, '', true);
-
-                            return new WP_REST_Response(
-                                [
-                                    'status' => 200,
-                                    'indexing_status' => 'failed',
-                                    'response' => $request->get_param('message')
-                                ]
-                            );
-                        }
-
-
-                        $multilanguage = Multilanguage::instance();
-                        $lang = ($multilanguage->get_current_language() === $multilanguage->get_base_language()) ? "" : $multilanguage->get_current_language();
-                        //Hide the indexing notice
-                        Setup_Wizard::dismiss_indexing_notice();
-                        Settings::set_indexing_status('processed', $lang);
-                        // Enable JS Layer for the indexed language
-                        Settings::enable_js_layer($lang);
-
-                        return new WP_REST_Response(
-                            [
-                                'status' => 200,
-                                'indexing_status' => Settings::get_indexing_status($lang),
-                                'response' => "Indexing status updated"
-                            ]
-                        );
-                    },
-                    'permission_callback' => '__return_true'
-                ));
+                REST_API_Handler::initialize();
+                Index_Status_Handler::initialize();
             });
         }
 
