b385464f25636677c6d6a8d2307c6423661856f1
davidmolinacano <davidmolinacano@gmail.com>
2023-10-26 12:24:32 +0200

    feat: Added local phplint

---
+++ phplint.sh
@@ -0,0 +1,14 @@
+
+if [ ! -d "./vendor" ] 
+then
+    composer install
+fi
+
+phpVersions=("5.6" "7.0" "7.2" "7.3" "7.4" "8.0" "8.2")
+
+for phpVersion in ${phpVersions[@]}; do
+    echo "---------------------------------------------"
+    echo "---- Checking compatibility with PHP ${phpVersion} ----"
+    echo "---------------------------------------------"
+    ./vendor/bin/phpcs -p . --standard=PHPCompatibilityWP --ignore=*/tests/*,*/vendor/* --extensions=php --runtime-set testVersion $phpVersion-
+done
