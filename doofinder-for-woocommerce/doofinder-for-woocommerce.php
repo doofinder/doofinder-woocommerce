264e72837fc007878159cd4712c36ceb93c09a07
Pedro <pedro@doofinder.com>
2023-12-01 08:42:43 +0000

    Bump up version

--- doofinder-for-woocommerce/doofinder-for-woocommerce.php
+++ doofinder-for-woocommerce/doofinder-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name: Doofinder WP & WooCommerce Search
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 2.0.33
+ * Version: 2.0.34
  * Requires at least: 5.6
  * Requires PHP: 7.0
  * Author: Doofinder
@@ -35,7 +35,7 @@ if (!class_exists('\Doofinder\WP\Doofinder_For_WordPress')) :
          *
          * @var string
          */
-        public static $version = '2.0.33';
+        public static $version = '2.0.34';
 
         /**
          * The only instance of Doofinder_For_WordPress
--- doofinder-for-woocommerce/readme.txt
+++ doofinder-for-woocommerce/readme.txt
@@ -1,7 +1,7 @@
 === Doofinder WP & WooCommerce Search ===
 Contributors: Doofinder
 Tags: search, autocomplete
-Version: 2.0.33
+Version: 2.0.34
 Requires at least: 5.6
 Tested up to: 6.3.1
 Requires PHP: 7.0
@@ -81,6 +81,9 @@ General Settings
 Just send your questions to <mailto:support@doofinder.com> and we will try to answer as fast as possible with a working solution for you.
 
 == Changelog ==
+
+= 2.0.34 =
+Added security improvements.
 
 = 2.0.33 =
 New functionality added: Conversion pages.
