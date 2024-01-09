2b32472d91676c90fe756daf620e23e720194090
vicfebo@gmail.com <vicfebo@gmail.com>
2024-01-09 18:13:19 +0100

    Adding indexation field

--- doofinder-for-woocommerce/doofinder-for-woocommerce.php
+++ doofinder-for-woocommerce/doofinder-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name: Doofinder WP & WooCommerce Search
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 2.1.13
+ * Version: 2.1.14
  * Requires at least: 5.6
  * Requires PHP: 7.0
  * Author: Doofinder
@@ -35,7 +35,7 @@ if (!class_exists('\Doofinder\WP\Doofinder_For_WordPress')) :
          *
          * @var string
          */
-        public static $version = '2.1.13';
+        public static $version = '2.1.14';
 
         /**
          * The only instance of Doofinder_For_WordPress
