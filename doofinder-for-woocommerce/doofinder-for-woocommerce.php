65bbc88078942e3b9593b2e75241dba7c1d096a5
davidmolinacano <davidmolinacano@gmail.com>
2024-03-11 13:26:27 +0100

    feat: version bump to 2.2.13

--- doofinder-for-woocommerce/doofinder-for-woocommerce.php
+++ doofinder-for-woocommerce/doofinder-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name: Doofinder WP & WooCommerce Search
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 2.2.12
+ * Version: 2.2.13
  * Requires at least: 5.6
  * Requires PHP: 7.0
  * Author: Doofinder
@@ -36,7 +36,7 @@ if (!class_exists('\Doofinder\WP\Doofinder_For_WordPress')) :
          * @var string
          */
 
-        public static $version = '2.2.12';
+        public static $version = '2.2.13';
 
         /**
          * The only instance of Doofinder_For_WordPress
