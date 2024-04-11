a71a1384ceb52fd08597d89eed0b4a7e4bd75fa9
eduardogomez97 <ryukedu86@gmail.com>
2024-04-11 16:12:29 +0200

    [Woocommerce] - Improve Update on Save API handle response

--- doofinder-for-woocommerce/includes/api/class-update-on-save-api.php
+++ doofinder-for-woocommerce/includes/api/class-update-on-save-api.php
@@ -91,12 +91,14 @@ class Update_On_Save_Api
 
         $response = wp_remote_request($url, $data);
 
-        if (!is_wp_error($response) && $response['response']['code'] === 200) {
+        if ($response['response']['code'] === 200) {
             $this->log->log("The update on save request has been processed correctly");
             return TRUE;
+        } elseif (!is_wp_error($response)) {
+            $this->log->log("Error in the request: " . print_r($response, true));  
         } else {
             $error_message = $response->get_error_message();
-            $this->log->log("Error in the request: $error_message");
+            $this->log->log("WP-Error in the request: $error_message");
         }
         return false;
     }
