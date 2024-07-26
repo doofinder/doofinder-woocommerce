6c5fcf07819c0639a8a060a67166791fd48338e3
Yunier López Martinez <yunier0525+yuloma@gmail.com>
2024-07-26 15:10:09 +0200

    upgrade version

--- doofinder-for-woocommerce/doofinder-for-woocommerce.php
+++ doofinder-for-woocommerce/doofinder-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name: DOOFINDER Search and Discovery for WP & WooCommerce
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 2.4.5
+ * Version: 2.4.6
  * Requires at least: 5.6
  * Requires PHP: 7.0
  * Author: Doofinder
@@ -35,7 +35,7 @@ if ( ! class_exists( '\Doofinder\WP\Doofinder_For_WordPress' ) ) :
 		 * @var string
 		 */
 
-		public static $version = '2.4.5';
+		public static $version = '2.4.6';
 
 		/**
 		 * The only instance of Doofinder_For_WordPress
