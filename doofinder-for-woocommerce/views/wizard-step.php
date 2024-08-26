f92d581d881cdfa4857ead868306bd59af3dfa3e
davidmolinacano <davidmolinacano@gmail.com>
2024-08-26 16:51:04 +0200

    feat: Removed extract and throw exclusion from phpcs.xml

--- doofinder-for-woocommerce/views/wizard-step.php
+++ doofinder-for-woocommerce/views/wizard-step.php
@@ -10,22 +10,23 @@ use Doofinder\WP\Settings;
 
 $wizard = Setup_Wizard::instance();
 
-$step     = isset( $step ) ? $step : 1;
-$active   = isset( $active ) ? $active : false;
-$active   = $step === $step_state;
-$finished = $step_state > $step;
-$df_error = $wizard->get_errors_html( "wizard-step-$step" );
+$step       = ! empty( $data['step'] ) ? $data['step'] : 1;
+$step_state = ! empty( $data['step_state'] ) ? $data['step_state'] : 1;
+$active     = isset( $active ) ? $active : false;
+$active     = $step === $step_state;
+$finished   = $step_state > $step;
+$df_error   = $wizard->get_errors_html( "wizard-step-$step" );
 ?>
 <div class="df-setup-step df-setup-step-connect <?php echo $active ? 'active' : ''; ?> <?php echo ! empty( $df_error ) ? 'has-error' : ''; ?> <?php echo $finished ? 'finished' : ''; ?>">
 	<span class="df-setup-step__number"><?php echo esc_html( $step ); ?></span>
 	<div class="df-setup-step__wrap">
 		<div class="df-setup-step__header">
-			<?php if ( isset( $title ) ) : ?>
-			<h3 class="df-setup-step__title"><?php echo esc_html( $title ); ?></h3>
+			<?php if ( ! empty( $data['title'] ) ) : ?>
+			<h3 class="df-setup-step__title"><?php echo esc_html( $data['title'] ); ?></h3>
 			<?php endif; ?>
 			
-			<?php if ( isset( $desc ) ) : ?>
-			<p class="df-setup-step__desc"><?php echo esc_html( $desc ); ?></p>
+			<?php if ( ! empty( $data['desc'] ) ) : ?>
+			<p class="df-setup-step__desc"><?php echo esc_html( $data['desc'] ); ?></p>
 			<?php endif; ?>
 		</div>
 		<div class="df-setup-step__content">
