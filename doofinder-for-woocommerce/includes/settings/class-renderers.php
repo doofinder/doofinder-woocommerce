e4d771c9d484362a245b40efe43b7a34f78a215d
davidmolinacano <davidmolinacano@gmail.com>
2024-12-10 08:53:15 +0100

    fix: Prevented an error initializing  array

--- doofinder-for-woocommerce/includes/settings/class-renderers.php
+++ doofinder-for-woocommerce/includes/settings/class-renderers.php
@@ -631,7 +631,10 @@ trait Renderers {
 
 		$default_image_sizes = get_intermediate_image_sizes();
 
+		$image_sizes = array();
+
 		foreach ( $default_image_sizes as $size ) {
+			$image_sizes[ $size ]           = array();
 			$image_sizes[ $size ]['width']  = intval( get_option( "{$size}_size_w" ) );
 			$image_sizes[ $size ]['height'] = intval( get_option( "{$size}_size_h" ) );
 			$image_sizes[ $size ]['crop']   = get_option( "{$size}_crop" ) ? get_option( "{$size}_crop" ) : false;
