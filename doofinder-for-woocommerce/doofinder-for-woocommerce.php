66c48b89b360ded0dd28144ce7fae7f8913fd67a
brunovesar <158461069+brunovesar@users.noreply.github.com>
2025-06-12 11:36:30 +0200

    Index type field for products (#382)

--- doofinder-for-woocommerce/doofinder-for-woocommerce.php
+++ doofinder-for-woocommerce/doofinder-for-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: DOOFINDER Search and Discovery for WP & WooCommerce
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 2.7.11
+ * Version: 2.7.12
  * Requires at least: 5.6
  * Requires PHP: 7.0
  * Author: Doofinder
@@ -41,7 +41,7 @@ if ( ! class_exists( '\Doofinder\WP\Doofinder_For_WordPress' ) ) :
 		 * @var string
 		 */
 
-		public static $version = '2.7.11';
+		public static $version = '2.7.12';
 
 		/**
 		 * The only instance of Doofinder_For_WordPress
